c7c95450f151bdaaa74af69dde28ebe1fee516b0
Lisa Rosselli <studio1809@gmail.com>
2015-02-15 21:02:00 -0600

    studying modules

---
+++ js/modules.js
@@ -0,0 +1,66 @@
+(function () {
+  console.log("hello world");
+  // an immediately-invoked-function-expression
+})();
+
+//http://toddmotto.com/mastering-the-module-pattern/
+
+var Module = (function() {
+  
+  var privateMethod1 = function() {
+    console.log("privateMethod1");
+  };
+  
+  return {
+    publicMethod: function() {
+      console.log("publicMethod");
+      privateMethod1();
+    }
+  };
+  
+})();
+
+var Module2 = (function() {
+  var obj = {};
+  
+  var privateMethod = function() {
+    
+  };
+  
+  obj.someMethod = function() {
+    console.log("someMethod is public");
+  }
+  
+  return obj;
+})();
+
+var Module3 = (function() {
+  var privateValue1 = "privateValue1 here!";
+  
+  var privateMethod = function() {
+    //private
+  };
+  
+  var publicMethod = function() {
+    //public
+    console.log(privateValue1);
+  };
+  
+  var anotherMethod = function() {
+    //public
+  };
+  
+  return {
+    publicMethod1: publicMethod,
+    publicMethod2: anotherMethod
+  }
+})();
+
+// extending a module
+var ModuleExt = (function(Module3) {
+  Module3.extension = function() {
+    console.log("extension function");
+  };
+  
+  return Module3;
+})(Module3);
